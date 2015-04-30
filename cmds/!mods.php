4fb22d9ea16eef12c94de9863d16dab24ecb4c1e
daeks <daeks@gmx.net>
2015-04-30 18:57:14 +0200

    hide owner and bot from mod list

--- cmds/!mods.php
+++ cmds/!mods.php
@@ -5,7 +5,15 @@
                'help' => 'Displays list of moderators');
   
   if($execute) {
-    $this->say($this->target, true, trim('- Mods of '.$this->target.': '.implode(' ', $this->db[$this->target]['config']['mods'])));
+    $mods = $this->db[$this->target]['config']['mods'];
+    foreach($mods as $name) {
+      if(in_array($name, $this->db['#'.strtolower(BOTNAME)]['config']['mods'])) {
+        $mods = $this->remove($mods, $name);
+      }
+    }
+    if(sizeof($mods) > 0) {
+      $this->say($this->target, true, trim('- Mods of '.$this->target.': '.implode(' ', $mods)));
+    }
   }
   
 ?>
