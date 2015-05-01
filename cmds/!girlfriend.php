14a02eb732dc2e9dc22b898c981b3d29117d93f8
daeks <daeks@gmx.net>
2015-05-01 21:51:17 +0200

    bug fixes

--- cmds/!girlfriend.php
+++ cmds/!girlfriend.php
@@ -8,7 +8,7 @@
     $this->say($this->target, true, '/color '.$this->colors[rand(0, sizeof($this->colors))]);
     sleep(1);
     $this->say($this->target, false, '@'.$this->username.' I have listed your request as possible girlfriend for '.ucfirst(ltrim($this->target, '#').' Kappa'));
-    $this->say($this->target, true, '/color '.$this->color);
+    $this->say($this->target, true, '/color '.COLOR);
   }
   
 ?>
--- cmds/!girlfriends.php
+++ cmds/!girlfriends.php
@@ -5,7 +5,7 @@
                'help' => 'Displays the amount of the girlfriend queue');
   
   if($execute) {
-    $subcommand = rtrim($this->data[3], 's');
+    $subcommand = rtrim($this->command, 's');
     $this->say($this->target, false, ucfirst(ltrim($this->target, '#')).' could choose between '.(isset($this->db[$this->target]['stats']['cmds'][$subcommand]) ? $this->db[$this->target]['stats']['cmds'][$subcommand] : 0).' possible girlfriends. This takes some time to evaluate. Please be patient. Kappa');
   }
   
